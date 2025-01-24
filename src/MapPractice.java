c46eda8353ade44cdc98e70e5f1e3ea73e67ef8c
Abdi <89213120+abdirashidexe@users.noreply.github.com>
2025-01-23 21:50:01 -0800

    progressing through MapPractice problems

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -1,16 +1,24 @@
-
+import java.util.Map;
+import java.util.HashMap;
 
 public class MapPractice {
   public static void main(String[] args) {
     // Create a HashMap with String keys and Integer values and
     // assign it to a variable of type Map
+    Map<String, Integer> myMap = new HashMap<String, Integer>();
 
     // Put 3 different key/value pairs in the Map
     // (it's OK to do this one-by-one)
+    myMap.put("item_0", 0);
+    myMap.put("item_1", 1);
+    myMap.put("item_2", 2);
+    System.out.println(myMap);
 
     // Get the value associated with a given key in the Map
+    System.out.println(myMap.get("item_1"));
 
     // Find the size (number of key/value pairs) of the Map
+    System.out.println(myMap.size());
 
     // Replace the value associated with a given key (the size of the Map shoukld not change)
 
