31e43bd6f3cd3553f1886b4ae8b73fe942e46749
Abdi <89213120+abdirashidexe@users.noreply.github.com>
2025-01-23 17:42:26 -0800

    finished NumberPractice, began ListPractice

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,17 +1,29 @@
+import java.util.ArrayList;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
+    ArrayList list = new ArrayList<String>();
 
     // Add 3 elements to the list (OK to do one-by-one)
+    list.add("i0");
+    list.add("i1");
+    list.add("i2");
 
     // Print the element at index 1
+    System.out.println(list.get(1));
 
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
+    list.set(1, "replaced");
+
+    System.out.println(list);
 
     // Insert a new element at index 0 (the length of the list will change)
+    //list.add(0, "New");
+    //System.out.println(list);
 
     // Check whether the list contains a certain string
 
--- toRefresh.md
+++ toRefresh.md
@@ -2,4 +2,8 @@
 
 As you work through this exercise, write down anything that you needed to look up or struggled to remember here. It can be just a word or two (e.g. "joining strings"). You can use this as a guide of what to make extra sure you're refreshed on before exams and interviews.
 
+1. Wrote double instead of float first
+2. ArrayList<> should be arraylist(String)
+3. Looked up java methods to find set()
+
 - 
